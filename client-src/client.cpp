dd7a0a929510c1dad362aaa2321f3e34d18d14cc
Lucas Lopes <lucas.blopes3@gmail.com>
2024-06-06 21:26:35 -0300

    Implements infinite wait loop in the server

--- client-src/client.cpp
+++ client-src/client.cpp
@@ -14,41 +14,43 @@
 using namespace std;
 
 int main() {
-	int sockfd;
-	struct sockaddr_ll server_addr;
+    int sockfd;
+    struct sockaddr_ll server_addr;
 
-	sockfd = create_raw_socket();
+    sockfd = create_raw_socket();
 
-	// Obtain the interface index for the specified network interface
-	const char *interfaceName = INTERFACE_NAME;
-	int interface_index = if_nametoindex(interfaceName);
-	if (interface_index == 0) {
-		perror("Error obtaining interface index: Check if the interface name is correct.");
-		exit(1);
-	}
+    // Obtain the interface index for the specified network interface
+    const char *interfaceName = INTERFACE_NAME;
+    int interface_index = if_nametoindex(interfaceName);
+    if (interface_index == 0) {
+        perror("Error obtaining interface index: Check if the interface name "
+               "is correct.");
+        exit(1);
+    }
 
-	server_addr.sll_family = AF_PACKET;
-	server_addr.sll_protocol = htons(ETH_P_ALL);
-	server_addr.sll_ifindex = interface_index;
+    server_addr.sll_family = AF_PACKET;
+    server_addr.sll_protocol = htons(ETH_P_ALL);
+    server_addr.sll_ifindex = interface_index;
 
-	socket_config(sockfd, 5, interface_index);
+    socket_config(sockfd, TIMEOUT_SECONDS, interface_index);
 
-	// set the frame
-	Frame frame;
-	frame.start_marker = 0x7E;
-	frame.length = 5;
-	frame.sequence = 0;
-	frame.type = 0xA;
-	strcpy((char *)frame.data, "Hello from client!");
-	frame.crc = calculate_crc(frame);
+    // set the frame
+    Frame frame;
+    frame.start_marker = 0x7E;
+    frame.length = 5;
+    frame.sequence = 0;
+    frame.type = 0xA;
+    strcpy((char *)frame.data, "Hello from client!");
+    frame.crc = calculate_crc(frame);
 
-	// Send the frame and wait for the responsee aguarda a resposta
-	if (send_frame_with_timeout(sockfd, frame, server_addr, sizeof(server_addr), 5)) {
-		cout << "Frame sent successfully" << endl;
-	} else {
-		cerr << "Failed to send the frame within the timeout period" << endl;
-	}
+    // Send the frame and wait for the responsee aguarda a resposta
+    if (send_frame_with_timeout(sockfd, frame, server_addr, sizeof(server_addr),
+                                TIMEOUT_SECONDS)) {
+        cout << "Frame sent successfully" << endl;
+    } else {
+        cerr << "Failed to send the frame within the timeout period" << endl;
+    }
 
-	close(sockfd);
-	return 0;
+    close(sockfd);
+    return 0;
 }
--- server-src/server.cpp
+++ server-src/server.cpp
@@ -14,72 +14,69 @@
 using namespace std;
 
 int main() {
-	int sockfd;
-	struct sockaddr_ll client_addr;
+    int sockfd;
+    struct sockaddr_ll client_addr;
 
-	sockfd = create_raw_socket();
+    sockfd = create_raw_socket();
 
-	// Obtain the interface index for the specified network interface
-	const char *interfaceName = INTERFACE_NAME;
-	int interface_index = if_nametoindex(interfaceName);
-	if (interface_index == 0) {
-		perror("Error obtaining interface index: Check if the interface name is correct.");
-		exit(1);
-	}
+    // Obtain the interface index for the specified network interface
+    const char *interface_name = INTERFACE_NAME;
+    int interface_index = if_nametoindex(interface_name);
+    if (interface_index == 0) {
+        perror("Error obtaining interface index: Check if the interface name "
+               "is correct.");
+        exit(1);
+    }
 
-	// Set up the socket address structure
-	struct sockaddr_ll address = {};
-	address.sll_family = AF_PACKET;
-	address.sll_protocol = htons(ETH_P_ALL);
-	address.sll_ifindex = interface_index;
+    // Set up the socket address structure
+    struct sockaddr_ll address = {};
+    address.sll_family = AF_PACKET;
+    address.sll_protocol = htons(ETH_P_ALL);
+    address.sll_ifindex = interface_index;
 
-	if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1) {
-		perror("bind");
-		close(sockfd);
-		exit(EXIT_FAILURE);
-	}
+    if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+        perror("bind");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
-	socket_config(sockfd, 5, interface_index);
+    socket_config(sockfd, TIMEOUT_SECONDS, interface_index);
 
-	Frame frame;
-	// Waits to receive a frame
-	if (receive_frame_with_timeout(sockfd, client_addr, frame, 5)) {
-		cout << "Received frame with data: " << frame.data << endl;
+    while (true) {
+        Frame frame;
+        socklen_t addr_len = sizeof(client_addr);
 
-		// (ACK) response
-		Frame ack;
-		ack.start_marker = 0x7E;
-		ack.length = 3;
-		ack.sequence = frame.sequence;
-		ack.type = 0;
-		strcpy((char *)ack.data, "ACK");
-		ack.crc = calculate_crc(ack);
+        ssize_t len = recvfrom(sockfd, &frame, sizeof(Frame), 0,
+                               (struct sockaddr *)&client_addr, &addr_len);
+        if (len > 0 && frame.start_marker == 0x7E) {
+            // Check CRC
+            unsigned char crc = calculate_crc(frame);
+            if (crc == frame.crc) {
+                cout << "Received frame with data: " << frame.data << endl;
 
-		ssize_t sent_bytes = sendto(sockfd, &ack, sizeof(Frame), 0, (struct sockaddr *)&client_addr,
-									sizeof(client_addr));
-		if (sent_bytes == -1) {
-			cerr << "Failed to send ACK: " << strerror(errno) << endl;
-		} else {
-			cout << "Sent ACK successfully!" << endl;
-		}
-	} else {
-		// (NACK) response
-		Frame nack;
-		nack.start_marker = 0x7E;
-		nack.length = 4;
-		nack.sequence = frame.sequence;
-		nack.type = 1;
-		strcpy((char *)nack.data, "NACK");
-		nack.crc = calculate_crc(nack);
+                // Send ACK
+                Frame ack;
+                ack.start_marker = 0x7E;
+                ack.length = 3;
+                ack.sequence = frame.sequence;
+                ack.type = 0;
+                strcpy((char *)ack.data, "ACK");
+                ack.crc = calculate_crc(ack);
 
-		if (sendto(sockfd, &nack, sizeof(Frame), 0, (struct sockaddr *)&client_addr,
-				   sizeof(client_addr) == -1)) {
-			cerr << "Failed to send NACK" << endl;
-		} else {
-			cerr << "Sent NACK successfully" << endl;
-		}
-	}
+                ssize_t sent_bytes = sendto(sockfd, &ack, sizeof(Frame), 0,
+                                            (struct sockaddr *)&client_addr,
+                                            sizeof(client_addr));
+                if (sent_bytes == -1) {
+                    cerr << "Failed to send ACK: " << strerror(errno) << endl;
+                } else {
+                    cout << "Sent ACK successfully!" << endl;
+                }
+            } else {
+                cerr << "CRC check failed" << endl;
+            }
+        }
+    }
 
-	close(sockfd);
-	return 0;
+    close(sockfd);
+    return 0;
 }
